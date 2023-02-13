8659bb9b624bef3fbf3dfbf3abf8bbbb25d2e7a0
indira Davoin <lunarte.mix@gmail.com>
2023-02-12 23:51:01 -0300

    estructura html+comiezo js css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+//Prueba de funcion de botones
+document.getElementById("generar").addeventlistener("click", function () {
+    alert("mensaje cifrado");
+});
+document.getElementById("descifrar").addeventlistener("click", function () {
+    alert("mensaje descifrado");
+});
+
