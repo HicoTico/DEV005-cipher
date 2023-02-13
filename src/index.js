4e4b8d38fab5f2f3dab02207d691c3b79730af30
indira Davoin <lunarte.mix@gmail.com>
2023-02-13 07:37:44 -0300

    2da parte html+inicios js css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
-//Prueba de funcion de botones
-document.getElementById("generar").addeventlistener("click", function () {
-    alert("mensaje cifrado");
+
+document.getElementById("getOffPass").style.display = "none";
+
+//document.getElementById("ingreseAqui").addEventListener("click", function () style.display("getOffPass");});
+//datos placeholder
+//var datoImportante = document.getElementById("datoUsuario").value;
+//var dato1 = datoImportante.charCodeAt(2);
+//console.log (dato1);
+
+//listener de botones
+document.getElementById("generar").addEventListener("click", function () 
+{alert("mensaje cifrado");
+document.getElementById("getOffPass").style.display = "block";
+document.getElementById("getPass").style.display = "none";
 });
-document.getElementById("descifrar").addeventlistener("click", function () {
-    alert("mensaje descifrado");
+
+document.getElementById("descifrar").addEventListener("click", function () 
+{alert("mensaje descifrado");
 });
+
+document.getElementById("otroCypher").addEventListener("click", function () 
+{history.go();
+});
+
 
