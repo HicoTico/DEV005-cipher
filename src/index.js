1d636c6731d8afcf0bcf7879067e147b242e1cbf
indira Davoin <lunarte.mix@gmail.com>
2023-02-21 16:43:19 -0300

    css listo/html listo/js falta veridificar formula de alritmo de descifrado

--- src/index.js
+++ src/index.js
@@ -17,34 +17,34 @@ document.getElementById('goDescifrar').addEventListener('click', function () {
 
 
 //listener de botones section y recoleccion de datos
-document.getElementById('datoUsuario').addEventListener('keyup', function(){
+document.getElementById('datoUsuario').addEventListener('keyup', function () {
   this.value = this.value.toUpperCase();
 });
 
-document.getElementById('get').addEventListener('click', function (){
-  const offset = document.getElementById ('rango').value;
-  const texto = document.getElementById ('datoUsuario').value;
+document.getElementById('get').addEventListener('click', function () {
+  const offset = document.getElementById('rango').value;
+  const texto = document.getElementById('datoUsuario').value;
   const resultado = cipher.encode(texto, parseInt(offset))
   alert('! E X C E L E N T E ! \n ...ya tienes tu CyPHER Pass \n' + resultado);
 });
 
-document.getElementById('datoUsuariooff').addEventListener('keyup', function(){
+document.getElementById('datoUsuariooff').addEventListener('keyup', function () {
   this.value = this.value.toUpperCase();
 });
 
-document.getElementById('descifrar').addEventListener('click', function (){
-  const offset = document.getElementById ('rangoOff').value;
-  const texto = document.getElementById ('datoUsuariooff').value;
+document.getElementById('descifrar').addEventListener('click', function () {
+  const offset = document.getElementById('rangoOff').value;
+  const texto = document.getElementById('datoUsuariooff').value;
   const resultado = cipher.decode(texto, parseInt(offset))
-  console.log ('resultado decode ' + resultado)
-  //alert('! E X C E L E N T E !\n ' + resultado);
+  //console.log ('resultado decode ' + resultado)
+  alert('! E X C E L E N T E !\n ' + resultado);
 });
 
-document.getElementById('otroCypher').addEventListener('click', function (){
+document.getElementById('otroCypher').addEventListener('click', function () {
   history.go();
 });
 
-document.getElementById('otroCypher2').addEventListener('click', function (){
+document.getElementById('otroCypher2').addEventListener('click', function () {
   history.go();
 });
 
